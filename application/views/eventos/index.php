7c6851ea9715ffe5761e52ebfe20345ae09ff119
zamocarlos <zamocarlos@hotmail.com>
2018-12-17 18:52:13 +0100

    cambios en el HTML de la vista

--- application/views/eventos/index.php
+++ application/views/eventos/index.php
@@ -2,8 +2,9 @@
 
 <?php foreach ($eventos as $eventos_item): ?>
 
-        <h3><?php echo $eventos_item['titulo']; ?></h3>
+        <h3>Título evento: </h3><h5><?php echo $eventos_item['titulo']; ?></h5>
         <div class="main">
+
                 <?php echo $eventos_item['tipo_evento']; ?>
         </div>
         <p><a href="<?php echo site_url('eventos/'.$eventos_item['id_evento']); ?>">Ver evento</a></p>
